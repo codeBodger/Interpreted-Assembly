da56412711f62e996dda818bc6970a9f3c8b4ed7
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-04-04 15:20:22 -0400

    Instructions for moving stuff

--- interpreter.c
+++ interpreter.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include "libsense/sense.h"
 
+#define ADDRESS2REG 0b00000111
+
 struct Line {
     unsigned int instruction : 5;
     unsigned int reg : 3;
@@ -23,16 +25,92 @@ int main() {
     register short bbb;
 
     register unsigned char line = 0;
+    register short valueInReg;
+    register short valueInReg1;
+    register short valueInReg2;
 
     while (1) {
+        switch (code[line].reg) {
+            case 0: valueInReg = aaa; break;
+            case 1: valueInReg = aab; break;
+            case 2: valueInReg = aba; break;
+            case 3: valueInReg = abb; break;
+            case 4: valueInReg = baa; break;
+            case 5: valueInReg = bab; break;
+            case 6: valueInReg = bba; break;
+            case 7: valueInReg = bbb; break;
+            default: return 1;
+        }
+        switch ((code[line].address >> 4) & ADDRESS2REG) {
+            case 0: valueInReg1 = aaa; break;
+            case 1: valueInReg1 = aab; break;
+            case 2: valueInReg1 = aba; break;
+            case 3: valueInReg1 = abb; break;
+            case 4: valueInReg1 = baa; break;
+            case 5: valueInReg1 = bab; break;
+            case 6: valueInReg1 = bba; break;
+            case 7: valueInReg1 = bbb; break;
+            default: return 1;
+        }
+        switch (code[line].address & ADDRESS2REG) {
+            case 0: valueInReg2 = aaa; break;
+            case 1: valueInReg2 = aab; break;
+            case 2: valueInReg2 = aba; break;
+            case 3: valueInReg2 = abb; break;
+            case 4: valueInReg2 = baa; break;
+            case 5: valueInReg2 = bab; break;
+            case 6: valueInReg2 = bba; break;
+            case 7: valueInReg2 = bbb; break;
+            default: return 1;
+        }
+
         switch (code[line].instruction) {
             case MOVE_: // move>regAddresss [from reg to Addresss]
+                memory[code[line].address] = valueInReg;
+            break;
             case MOVE1: // move<regAddresss [from Addresss to reg]
+                switch (code[line].reg) {
+                    case 0: aaa = memory[code[line].address]; break;
+                    case 1: aab = memory[code[line].address]; break;
+                    case 2: aba = memory[code[line].address]; break;
+                    case 3: abb = memory[code[line].address]; break;
+                    case 4: baa = memory[code[line].address]; break;
+                    case 5: bab = memory[code[line].address]; break;
+                    case 6: bba = memory[code[line].address]; break;
+                    case 7: bbb = memory[code[line].address]; break;
+                }
+            break;
             case PUT__: // put<<reg________Value... [value into reg]
+                switch (code[line].reg) {
+                    case 0: aaa = code[++line]; break;
+                    case 1: aab = code[++line]; break;
+                    case 2: aba = code[++line]; break;
+                    case 3: abb = code[++line]; break;
+                    case 4: baa = code[++line]; break;
+                    case 5: bab = code[++line]; break;
+                    case 6: bba = code[++line]; break;
+                    case 7: bbb = code[++line]; break;
+                }
+            break;
             case PUT1_: // put<____AddresssValue... [value into Addresss]
+                memory[code[line].address] = code[++line];
+            break;
         
             case MOVE2: // mov>>regAddresss [from address in (char)reg to Address]
+                memory[code[line].address] = memory[(char)valueInReg];
+            break;
             case MOVE3: // mov<<reg____.reg [from address in (char).reg to reg]
+                switch (code[line].reg) {
+                    case 0: aaa = valueInReg3; break;
+                    case 1: aab = valueInReg3; break;
+                    case 2: aba = valueInReg3; break;
+                    case 3: abb = valueInReg3; break;
+                    case 4: baa = valueInReg3; break;
+                    case 5: bab = valueInReg3; break;
+                    case 6: bba = valueInReg3; break;
+                    case 7: bbb = valueInReg3; break;
+                }
+            break;
             case SHFTL: // <<___reg.reg-reg [<<, like below]
             case SHFTR: // >>___
 
